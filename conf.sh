1e62e533803fc92b43f9074222ad966f0054ad8f
Iago Okuma <92878748+okuma1@users.noreply.github.com>
2022-10-30 17:49:44 -0300

    Update conf.sh

--- conf.sh
+++ conf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 #
-if [ $EUID -ne 0 ] #conferindo se é o root ou nao. ID root (0)
+if [ "$EUID" -ne 0 ] #conferindo se é o root ou nao. ID root (0)
     then
     echo "Você não tem permissão para executar"
     exit 1
